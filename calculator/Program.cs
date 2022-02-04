35ef3b2986debaf71edd13a00d4a0a4193df1baf
Levente Boros <2021291@dszcberegszaszi.hu>
2022-02-04 11:40:42 +0100

    legnagyobb számológép ever

---
+++ calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //valtozok
+            int szam1;
+            int szam2;
+            char muvjel;
+            float eredm = 0;
+            string uzen = "a művelet eredménye";
+            //Beolvasok
+            szam1 = int.Parse(Console.ReadLine());
+            szam2 = int.Parse(Console.ReadLine());
+            muvjel = Convert.ToChar(Console.ReadLine());
+
+            switch(muvjel)
+            {
+                case '+':
+                    eredm = szam1 + szam2;
+                    break;
+                case '-':
+                    eredm = szam1 - szam2;
+                    break;
+                case '*':
+                    eredm = szam1 * szam2;
+                    break;
+                case '/':
+                    eredm = szam1 / szam2;
+                    break;
+                default:
+                    uzen = "hibás műveletijel";
+                    break;
+            }
+            Console.WriteLine(uzen + eredm);
+            Console.ReadKey();
+        }
+    }
+}
