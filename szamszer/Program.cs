07132838f9ecd64d122863f03f392485bd120df8
Levente Boros <2021291@dszcberegszaszi.hu>
2022-02-09 13:25:20 +0100

    ds

--- szamszer/Program.cs
+++ szamszer/Program.cs
@@ -10,8 +10,26 @@ namespace szamszer
     {
         static void Main(string[] args)
         {
+            int alsohatar = 1,
+                felsohatar = 101,
+                probal = 5,
+                gondoltszam,
+                tipp;
+                
+            Random rnd = new Random();
+            do
+            {
+                Console.WriteLine("Leszel az aki kitalálja?")
+                if (Console.ReadKey().KeyChar == 'i')
+                {
+                    gondoltszam = rnd.Next(alsohatar, felsohatar);
+                    for(int i = 0; i <= probal; i++)
+                }
+
+                Console.WriteLine("Folytatjuk? (i/n)");
+            } while(Console.ReadKey().KeyChar == 'i');
+            Console.ReadKey();
 
         }
-        function int Computer
     }
 }
