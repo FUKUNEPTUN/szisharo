45da13b8fcf760d99029f0f42072f4c1b7f14f76
Boros Levente <88634980+FUKUNEPTUN@users.noreply.github.com>
2022-02-17 10:47:06 +0100

    asda
    sad

--- szamtomb/Program.cs
+++ szamtomb/Program.cs
@@ -10,32 +10,32 @@ namespace szamtomb
     {
         static void Main(string[] args)
         {
-            int min = 1000,
-                max = -1,
-                minIdx,
-                maxIdx;
-            int[] szam = new int[1000];
-            Random rna = new Random();
+            int min = 1000, //ami ennél kisebb
+                max = -1, //ami ennel nagyobb 
+                minIdx = 0, //legkisebb számnak indexe
+                maxIdx = 0; // legnagyobb szám indexe
+            int[] szam = new int[1000]; //létrehozom a tömb objektumot
+            Random rna = new Random(); //létrehozok egy random objektumot
 
-            for(int i = 0; i <= 99; i++)
+            for(int i = 0; i <= 99; i++) // feltöltöm a számokat a tömbbe
             {
-                szam[i] = rna.Next(0, 1001);
+                szam[i] = rna.Next(0, 1001); //0-1000ig generált számot generáltunk és a tömbbe beleteszem
             }
-            for(int i = 0; i <=99; i++)
+            for(int i = 0; i <=99; i++) // végig lépkedünk a tömb elemein, hogy megkeressem a legkisebb és legnagyobb elemét a tömbnek
             {
-                if (szam[i] < min)
+                if (szam[i] < min)  //ha tömb eleme kisebb mint min akkor..
                 {
-                    min = szam[i];
-                    minIdx = i;
+                    min = szam[i]; //elmentem az értéket a min változóba és...
+                    minIdx = i;// min indexet a minIdx változóba
                 }
-                if (szam[i] > max)
+                if (szam[i] > max)//ha tömb eleme nagyobb mint max akkor..
                 {
-                    max = szam[i];
-                    maxIdx = i;
+                    max = szam[i];// elmentem az értéket a max változoba és..
+                    maxIdx = i;//az aktuális indexet a maxIdx-be
                 }
             }
 
-            Console.WriteLine(" értéke ", min, " Legnagobb érték ", max);
+            Console.WriteLine("Legkisebb érték, " + min +" indexe " + minIdx + " Legnagobb érték, " + max + " indexe "+ maxIdx); //az eredményt kiírattatom 
             Console.ReadKey();
             
         }
