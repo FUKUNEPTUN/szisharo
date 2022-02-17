962baf7d672a7431ad71a2f124088798d0183425
Levente Boros <2021291@dszcberegszaszi.hu>
2022-02-17 09:10:36 +0100

    kiskaracson

---
+++ szamtomb/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamtomb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] szam = new int[100],
+                  r;
+            Random rna = new Random();
+
+            for(int i = 0; i <= 100; i++)
+            {
+                r == 
+            }
+        }
+    }
+}
