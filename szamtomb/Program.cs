7a081f73816d82cd866b53616d19debc67a9e108
Levente Boros <2021291@dszcberegszaszi.hu>
2022-02-17 10:00:20 +0100

    1

--- szamtomb/Program.cs
+++ szamtomb/Program.cs
@@ -10,14 +10,34 @@ namespace szamtomb
     {
         static void Main(string[] args)
         {
-            int[] szam = new int[100],
-                  r;
+            int min = 1000,
+                max = -1,
+                minIdx,
+                maxIdx;
+            int[] szam = new int[1000];
             Random rna = new Random();
 
-            for(int i = 0; i <= 100; i++)
+            for(int i = 0; i <= 99; i++)
             {
-                r == 
+                szam[i] = rna.Next(0, 1001);
             }
+            for(int i = 0; i <=99; i++)
+            {
+                if (szam[i] < min)
+                {
+                    min = szam[i];
+                    minIdx = i;
+                }
+                if (szam[i] > max)
+                {
+                    max = szam[i];
+                    maxIdx = i;
+                }
+            }
+
+            Console.WriteLine(" értéke ", min, " Legnagobb érték ", max);
+            Console.ReadKey();
+            
         }
     }
 }
