e686865fb9534494a4048a7b7543221385704935
Levente Boros <2021291@dszcberegszaszi.hu>
2022-02-23 10:14:41 +0100

    doga

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = Convert.ToInt32(Console.ReadLine()),
+                a = Convert.ToInt32(Console.ReadLine()); 
+            while(i <= a)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
