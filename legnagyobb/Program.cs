4a53ec3c04aa9559c641a81b2d01cb6fa31b59fe
Levente Boros <2021291@dszcberegszaszi.hu>
2022-02-23 09:57:27 +0100

    doga

---
+++ legnagyobb/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace legnagyobb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 1, a2 = 0, a3 = 0;
+            while(a1 != 0)
+            {
+                a1 = Convert.ToInt32(Console.ReadLine());
+                if (a1 > a2)
+                {
+                    a2 = a1;
+                }
+            }
+            Console.WriteLine("A bekért számok közül a legnagyobb a {0} volt", a2);
+            Console.ReadKey();
+        }
+    }
+}
