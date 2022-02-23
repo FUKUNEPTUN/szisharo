63058282dd2c0f38bbd8d373191caacf48008944
Levente Boros <2021291@dszcberegszaszi.hu>
2022-02-23 11:39:13 +0100

    duda

---
+++ testtomegindex/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testtomegindex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg a testtomeget(kg)");
+            double testtomeg = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg a testmagassagot(cm)");
+            double testmagassag = Convert.ToDouble(Console.ReadLine());
+            double bmi = 0;        
+            testmagassag = testmagassag / 100;
+            bmi = testtomeg / (testmagassag * testmagassag);
+
+            Console.WriteLine(bmi);
+            if (bmi <= 18.5)
+            {
+                Console.WriteLine("sovány");
+            }
+            else if (bmi >= 18.6 && bmi <= 25)
+            {
+                Console.WriteLine("normál");
+            }
+            else if (bmi >= 26 && bmi <= 30)
+            {
+                Console.WriteLine("kövér");
+            }
+            else if (bmi > 30)
+            {
+                Console.WriteLine("dagadt");
+            }
+            Console.ReadKey();
+        }
+    }
+}
