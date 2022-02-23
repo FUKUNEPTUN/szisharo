42856c1fdd23cce1935788d3b44aef7cc278c369
Levente Boros <2021291@dszcberegszaszi.hu>
2022-02-23 11:21:53 +0100

    sad

---
+++ ponttavolsag/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponttavolsag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg az x1 pontot: ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg az x2 pontot: ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg az y1 pontot: ");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg az y2 pontot: ");
+            double y2 = double.Parse(Console.ReadLine());
+            double tav = 0;
+            tav = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+
+            Console.WriteLine("Két pont távolsága:{0}", tav);
+            Console.ReadKey();
+        }
+    }
+}
