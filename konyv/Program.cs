fc5fd24b06afe936f5e68f56ee482dc3d0be2856
Levente Boros <2021291@dszcberegszaszi.hu>
2022-02-23 09:30:26 +0100

    doga

---
+++ konyv/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "a", k2;
+            int k1 = 0;
+
+            while (a != "")
+            {
+                Console.WriteLine("Add meg a könyv címét:  ");
+                k2 = Console.ReadLine();
+                Console.WriteLine("Add meg a könyv oldalai számát:  ");
+                k1 = Convert.ToInt32(Console.ReadLine());
+                if(k1 < 150)
+                {
+                    Console.WriteLine("A(z) {0} rövid terjedelmű könyv", k2);
+                }
+                else if (k1 > 150)
+                {
+                    Console.WriteLine("A(z) {0} hosszú terjedelmű könyv", k2);
+                }
+                else if (k1 == 150)
+                {
+                    Console.WriteLine("A könyved pont 150 oldalas");
+                }
+                else
+                {
+                    Console.WriteLine("Valami galiba van");
+                }
+                Console.WriteLine("Szeretnél könyvet tesztelni? Ha nem csak nyomj egy entert");
+                a = Console.ReadLine();
+            }
+        }
+    }
+}
