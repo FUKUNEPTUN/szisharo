f8ee064cfcec49daf73b94984ad79f4b525cc500
Levente Boros <2021291@dszcberegszaszi.hu>
2022-03-18 11:09:50 +0100

    fg

--- halmaz/Program.cs
+++ halmaz/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* A és B típusú egész tömböket kell létrehozni.
+ Ezen tömbök elemeinek számát a felhasználó adja meg; ez 200-nál nagyobb nem lehet!
+ Amennyiben nagyobb abban az esetben a program generál egy számot 100 és 200 között és ez lesz az elemek száma.
+ A tömbök elemeinek értéke a [-100, 100] intervallumon kell, hogy szerepeljen! (random generált számok)
+
+ Metódusok:
+ Unió
+ Metszet
+ A-B halmaz
+ B-A halmaz*/
 namespace halmaz
 {
     class halmazok
@@ -18,31 +28,65 @@ namespace halmaz
             Console.WriteLine("Hány elem szerepeljen?");
             int szam = Convert.ToInt32(Console.ReadLine());
             Random rnd = new Random();
-            int[] a = new int[] {rnd.Next(-100, 100)};
-            int[] b = new int[] {rnd.Next(-100, 100)};
-            if (szam >= 200)
+            int[] a;
+            int[] b;
+            int[] metszet;
+            int[] aUnio;
+            int[] bUnio;
+
+            if (szam <200)
             {
+                 a = new int[szam];
+                 b = new int[szam];
                 for (int i = 0; i < szam; i++)
                 {
-                     int af= a[i];
-                     int bf = b[i];
+                    a[i] = rnd.Next(1, 200);
+                    b[i] = rnd.Next(1, 200);
+                 /* Console.WriteLine("A tömb: {0} /// B tömb {1}", a[i], b[i]);      */
+
                 }
             }
-            Console.WriteLine(af);
-            Console.WriteLine(bf);
-            /*  else
-              {
-                 for (int i = 0; i < rnd.Next(1, 10); i++)
-                  {
-                      int af = a[i];
-                     int bf = b[i];
-                      Console.WriteLine(af);
-                      Console.WriteLine(bf);
-                  } 
 
-              }
 
-              */
+            else
+            {
+                szam = rnd.Next(100, 200);
+                 a = new int[szam];
+                 b = new int[szam];
+                for (int i = 0; i < szam; i++)
+                {
+                    a[i] = rnd.Next(1, 200);
+                    b[i] = rnd.Next(1, 200);
+                 /* Console.WriteLine("index {0}", i);
+                    Console.WriteLine("A tömb: {0} /// B tömb {1}", a[i], b[i]); */
+                }
+            }
+
+
+
+            for (int i = 0; i < szam;i++)
+            {
+                for (int j = 0; j < szam; j++)
+                {
+                    if (a[i] == b[j])
+                    {
+                        metszet = new int[a[i]];
+                        Console.WriteLine("közös elem {0}", metszet[i]);
+                    }
+                    else
+                    {
+                        aUnio = new int[a[i]];
+                        
+                    }
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine(" p o r m e u o t ");
+            Console.WriteLine();
             Console.ReadKey();
         }
     }
