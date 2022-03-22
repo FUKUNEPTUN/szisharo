3e17fc4262d72ce4bf00847dbcfda713e549bc16
Levente Boros <2021291@dszcberegszaszi.hu>
2022-03-22 10:00:53 +0100

    evszakokok

---
+++ homersekletElemzes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homersekletElemzes
+{
+    class Homer
+    {
+        private int[] ho;
+        private string[] evszakok = new string[] { "tél", "tavasz", "nyár","ősz"};
+        private string evszak, honap;
+        private string[] honapok = new string[] { "Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December" };
+        private Random rnd = new Random();
+        public void bekeres() { }
+        public void datum()
+        {
+            //switch (Console.ReadLine())
+            //{
+            //    case "január":
+
+            //        evszak = evszakok[0];
+            //        honap = honapok[0];
+            //        switch ()
+            //        {
+
+            //        }
+                    break;
+            //}
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
