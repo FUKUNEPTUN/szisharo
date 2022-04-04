a5886f3c6315233f51df05064ef69ff4d790f693
Boros Levente <ostrom251@gmail.com>
2022-04-04 20:54:49 +0200

    Remélem ötöst kapok, kell az 59ezres ösztöndíj :)

---
+++ Elagazas/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elagazas
+{
+    class Elagaz
+    {
+        public Elagaz() { }
+        List<int> lista, paros;
+        Random random = new Random();
+        int[] tomb = new int[] { };
+        public void ficko()
+        {
+            lista = new List<int>();
+            paros = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                lista.Add(random.Next(0, 100));
+            }
+            Console.WriteLine("Lista elemei:");
+            foreach (var szam in lista)
+            {
+                Console.WriteLine("Elem: {0}", szam);
+                if (szam % 2 == 0)
+                {
+                    paros.Add(szam);
+                }
+            }
+            Console.WriteLine("Páros számok a listában:");
+            foreach (var aa in paros)
+            {
+                Console.WriteLine("{0}", aa);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elagaz gaz = new Elagaz();
+            gaz.ficko();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Listak/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listak
+{
+    class Zero
+    {
+        Random random = new Random();
+        public Zero() { }
+        public void vs()
+        {
+            Console.WriteLine("Hány elemű legyen a lista?"); 
+            
+            int[] lista = new int[Convert.ToInt32(Console.ReadLine())];
+            Console.Clear();
+            for (int i = 0; i < lista.Length; i++)
+            {
+                lista[i] = random.Next(10,100);
+            }
+
+            Console.WriteLine("Index:\t\t||\tindexen álló érték: ");
+
+            Console.WriteLine("*******************************************");
+            for (int i = 0; i < lista.Length; i++)
+            {
+                Console.WriteLine("\t {0}\t||\t {1}", i,lista[i]);
+            }
+        }
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zero zero = new Zero();
+            zero.vs();
+            Console.ReadKey();
+        }
+    }
+}
