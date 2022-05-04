4228267f1cd23200cca9ff73c12e04eaef14ddbb
Levente Boros <2021291@dszcberegszaszi.hu>
2022-05-04 14:55:48 +0200

    Remélem ötöst kapok, kell az 59ezres ösztöndíj :)

---
+++ Oszthat/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Oszthat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Ehes eh = new Ehes();
+            eh.setSzam();
+            eh.getSzam();
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ OszthatoTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OszthatoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OszthatoTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3a28d624-2654-49fe-b7e1-cafb13317efc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
