ad1cea5cd866fd9ef7056a09cee3baaff53f2688
Levente Boros <2021291@dszcberegszaszi.hu>
2022-05-17 12:22:18 +0200

    as

--- Henger3Teszt/UnitTest1.cs
+++ Henger3Teszt/UnitTest1.cs
@@ -7,12 +7,14 @@ namespace Henger3Teszt
     [TestClass]
     public class UnitTest1
     {
+
+        Hengerke hengerke = new Hengerke(); 
         [TestMethod]
         public void TestgetTerulet()
         {
             //AAA
             //Arrange
-            Hengerke hengerke = new Hengerke();
+            
             double vartEredmeny = 12.57 , kapottEredmeny;
             hengerke.setMagassag(3);
             hengerke.setSugar(2);
@@ -20,6 +22,36 @@ namespace Henger3Teszt
             //Action
             kapottEredmeny = hengerke.getTerulet();
 
+            //Assert
+            Assert.AreEqual(kapottEredmeny, vartEredmeny);
+        }
+        [TestMethod] 
+        public void TestgetKerulet()
+        {
+            double vartEredmeny =12.57 , kapottEredmeny;
+            hengerke.setMagassag(3);
+            hengerke.setSugar(2);
+            kapottEredmeny = hengerke.getKerulet();
+            //Assert
+            Assert.AreEqual(kapottEredmeny, vartEredmeny);
+        }
+        [TestMethod]
+        public void TestgetTerfogat()
+        {
+            double vartEredmeny = 12.57*3, kapottEredmeny;
+            hengerke.setMagassag(3);
+            hengerke.setSugar(2);
+            kapottEredmeny = hengerke.getTerfogat();
+            //Assert
+            Assert.AreEqual(kapottEredmeny, vartEredmeny);
+        }
+        [TestMethod]
+        public void TestgetFelszin()
+        {
+            double vartEredmeny = 62.85, kapottEredmeny;
+            hengerke.setMagassag(3);
+            hengerke.setSugar(2);
+            kapottEredmeny = hengerke.getFelszin();
             //Assert
             Assert.AreEqual(kapottEredmeny, vartEredmeny);
         }
