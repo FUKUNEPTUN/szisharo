0eb28b9757670a0d48ce679224e2ba2cf73f8490
Boros Levente <ostrom251@gmail.com>
2022-05-25 22:50:35 +0200

    Remélem ötös lesz, nagyob kell az a 59 ezer :(

--- BaratLapoz/Program.cs
+++ BaratLapoz/Program.cs
@@ -1,58 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BaratLapoz
 {
-    class Alfa
+    class BaratLapoz
     {
+        //================================================================== változók =====================================================================
         private string nev;
-        private DateTime szuletes;
-        private char nem;
-        private int buli, tovabb = 0;
-        private string sor; // Egy sor beolvasása
-        private string[] mezo; // Sor szétbontása
+        private DateTime szuletesi_datum;
+        private char neme;
+        private int bulizasi_hajlam , tovabb = 0;
+        private string s;
+        private string[] ss;
 
 
 
-        public Alfa() { }
+        public BaratLapoz() { }
 
-        public Alfa(string nev, DateTime szuletes, char nem, int buli)
+        public BaratLapoz(string nev, DateTime szuletesi_datum, char neme, int bulizasi_hajlam )
         {
             this.nev = nev;
-            this.szuletes = szuletes;
-            this.nem = nem;
-            this.buli = buli;
+            this.szuletesi_datum = szuletesi_datum;
+            this.neme = neme;
+            this.bulizasi_hajlam  = bulizasi_hajlam ;
         }
+        //==================================================================== Kiíratáshoz használt metódus =======================================================================================
 
         public void printBarat()
         {
-            Console.WriteLine("{0,-20} {1,-10} {2,1} {3}", this.nev, this.szuletes.ToShortDateString(), this.nem, this.buli);
+            Console.WriteLine("{0,-20} {1,-10} {2,1} {3}", this.nev, this.szuletesi_datum.ToShortDateString(), this.neme, this.bulizasi_hajlam ); //Consolera soronként kiirja az adatokat
         }
-        private List<Alfa> baratLista = new List<Alfa>(); // Ebben tároljuk a barát dokumentumot + lista létrehozás
+        private List<BaratLapoz> baratLista = new List<BaratLapoz>();
+        //==================================================================== Fájl kezelés =======================================================================================
+
         public void bekeres()
         {
-            // Féjlnév beolvasása
-            // Beolvasás fájlból
-            using (StreamReader sr = new StreamReader("baratlapoz.txt", Encoding.Default))
-                while (sr.EndOfStream == false)
+            //*************Ha nem létezik a barat.txt, akkor géppel legeneráltatom a filet, hogy ne nekem kelljen ezzel foglalkozni, persze igény szerint módosíthatóak az adatok*****
+            if (!File.Exists("barat.txt"))
+            {
+                Random r = new Random();
+                int k;
+                // Create a file to write to.
+                using (StreamWriter wr = File.CreateText("barat.txt"))
                 {
-                    sor = sr.ReadLine();
-                    if (sor.Trim().Length == 0) continue; // Ha üres a sor, akkor léptetünk
+                    for (int i = 0; i < 160; i++)
+                    {
+                        k = r.Next(1, 10);
+                        wr.WriteLine("a {0},2001.02.10,F,{1}", i, k);
+                    }
+                }
+            }
+            //****************************************** Beolvasott filenak darabolása és listába rendezése *****************************************************************************
+            using (StreamReader r = new StreamReader("barat.txt", Encoding.Default))
+                while (r.EndOfStream == false)
+                {
+                    s = r.ReadLine();
+                    if (s.Trim().Length == 0) continue; // üres sorok kiszűrése
+                    ss = s.Split(',');
 
-                    mezo = sor.Split('|'); // Szétbontás
-
-                    // Hozzáadás a listához
-                    baratLista.Add(new Alfa(
-                        mezo[0],
-                        DateTime.Parse(mezo[1]),
-                        char.Parse(mezo[2]),
-                        int.Parse(mezo[3])));
+                    baratLista.Add(new BaratLapoz(
+                        ss[0],
+                        DateTime.Parse(ss[1]),
+                        char.Parse(ss[2]),
+                        int.Parse(ss[3])));
                 }
         }
+        //==================================================================== Lapozhatóságot biztosító metódus  =======================================================================================
+
         public void Kiir()
         {
             var page1 = baratLista.GetRange(0, 20);
@@ -60,45 +78,69 @@ namespace BaratLapoz
             var page3 = baratLista.GetRange(40, 20);
             var page4 = baratLista.GetRange(60, 20);
             var page5 = baratLista.GetRange(80, 20);
+            var page6 = baratLista.GetRange(100, 20);
+            var page7 = baratLista.GetRange(120, 20);
             do
             {
                 if (tovabb == 0)
                 {
-                    foreach (Alfa key in page1)
+                    foreach (BaratLapoz key in page1)
                     {
                         key.printBarat();
                     }
                 }
                 else if (tovabb == 1)
                 {
-                    foreach (Alfa key in page2)
+                    foreach (BaratLapoz key in page2)
                     {
                         key.printBarat();
                     }
                 }
                 else if (tovabb == 2)
                 {
-                    foreach (Alfa key in page3)
+                    foreach (BaratLapoz key in page3)
                     {
                         key.printBarat();
                     }
                 }
                 else if (tovabb == 3)
                 {
-                    foreach (Alfa key in page4)
+                    foreach (BaratLapoz key in page4)
                     {
                         key.printBarat();
                     }
                 }
                 else if (tovabb == 4)
                 {
-                    foreach (Alfa key in page5)
+                    foreach (BaratLapoz key in page5)
                     {
                         key.printBarat();
                     }
                 }
+                else if (tovabb == 5)
+                {
+                    foreach (BaratLapoz key in page6)
+                    {
+                        key.printBarat();
+                    }
+                }
+                else if (tovabb == 6)
+                {
+                    foreach (BaratLapoz key in page7)
+                    {
+                        key.printBarat();
+                    }
+                }
+                else if (tovabb == -1)
+                {
+                    Console.WriteLine("még 1 page up és a program kilép");
+                }
+                else if (tovabb == 7)
+                {
+                    Console.WriteLine("még 1 page down és a program kilép");
+                }
                 var gomb = Console.ReadKey(true);
-                if (gomb.Key == ConsoleKey.PageUp)
+                if (gomb.Key == ConsoleKey.PageDown)
                 {
                     Console.Clear();
                     tovabb = tovabb + 1;
@@ -113,23 +155,28 @@ namespace BaratLapoz
                     Console.Clear();
                     tovabb = 4;
                 }
-                else if (gomb.Key == ConsoleKey.PageDown)
+                else if (gomb.Key == ConsoleKey.PageUp)
                 {
                     Console.Clear();
                     tovabb = tovabb - 1;
                 }
                 else { Console.Clear(); continue; }
-            } while (tovabb > -1 && tovabb < 5); // Ha a tovább kisebb mint 0 vagy nagyonbb mint 4 akkor a program kilép
+
+            } while (tovabb > -2 && tovabb < 8);
+            
         }
     }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Alfa bl = new Alfa();
-            bl.bekeres();
-            bl.Kiir();
+            BaratLapoz baratLapoz = new BaratLapoz();
+           
+             
+            baratLapoz.bekeres();
+            baratLapoz.Kiir();
         }
     }
 }
+
+
---
+++ BaratNagyBuli/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuliszervezesB
+{
+    class BLapoz
+    {
+        private string nev;
+        private DateTime szuletes;
+        private char nem;
+        private int buli;
+        private string sor; // Egy sor beolvasása
+        private string[] mezo; // Sor szétbontása
+
+
+
+        public BLapoz() { }
+
+        public BLapoz(string nev, DateTime szuletes, char nem, int buli)
+        {
+            this.nev = nev;
+            this.szuletes = szuletes;
+            this.nem = nem;
+            this.buli = buli;
+        }
+
+        public void printBarat()
+        {
+            Console.WriteLine("{0,-20} {1,-10} {2,1} {3}", this.nev, this.szuletes.ToShortDateString(), this.nem, this.buli);
+        }
+        private List<BLapoz> baratLista = new List<BLapoz>(); // Ebben tároljuk a barát dokumentumot + lista létrehozás
+        private List<BLapoz> Blist20 = new List<BLapoz>();// Barátok akik 20 évnél idősebbek
+        private List<BLapoz> Blist20DESC = new List<BLapoz>();// Barátok akik 20 évnél idősebbek bulizási hajlam szerint csökkenő sorrend
+        
+        public void bekeres()
+        {
+
+            if (!File.Exists("baratlapoz.txt"))
+            {
+                Random r = new Random();
+                int k, é, h, n;
+                // Create a file to write to.
+                using (StreamWriter wr = File.CreateText("baratlapoz.txt"))
+                {
+                    for (int i = 0; i < 160; i++)
+                    {
+                        k = r.Next(1, 10);
+                        é = r.Next(2000, 2005);
+                        h = r.Next(1, 12);
+                        n = r.Next(1, 30);
+                        if (h == 2)
+                        {
+                            n = r.Next(1, 28);
+                        }
+                        wr.WriteLine("{0}. Erzsébet Királynő,{2}.{3}.{4},n,{1}", i, k, é, h, n);
+                    }
+                }
+            }
+            // Féjlnév beolvasása
+            // Beolvasás fájlból
+            using (StreamReader sr = new StreamReader("baratlapoz.txt", Encoding.UTF8))
+                while (sr.EndOfStream == false)
+                {
+                    sor = sr.ReadLine();
+                    if (sor.Trim().Length == 0) continue; // Ha üres a sor, akkor léptetünk
+
+                    mezo = sor.Split(','); // Szétbontás
+
+                    // Hozzáadás a listához
+                    baratLista.Add(new BLapoz(
+                        mezo[0],
+                        DateTime.Parse(mezo[1]),
+                        char.Parse(mezo[2]),
+                        int.Parse(mezo[3])));
+                    if (DateTime.Parse(mezo[1]) < DateTime.Parse("2002.05.21") && int.Parse(mezo[3]) >= 5)
+                    {
+                        Blist20.Add(new BLapoz(
+                        mezo[0],
+                        DateTime.Parse(mezo[1]),
+                        char.Parse(mezo[2]),
+                        int.Parse(mezo[3])));
+                    }
+                }
+        }
+        public void Kiir()
+        {
+            Console.WriteLine("Barátaink:");
+            foreach (BLapoz key in baratLista)
+            {
+                key.printBarat();
+            }
+            Console.WriteLine();
+            Console.WriteLine("A buliba meghívható barátaink neve:");
+            int k = 0;
+            var newList = Blist20.OrderByDescending(x => x.buli).ToList(); // csökken bulizási hajlam szerint
+            if (Blist20.Count >= 10){
+                if (Blist20.Count > 15)
+                {
+                    
+                    for (int i = 0; i < 15; i++)
+                    {
+                        
+                        k++;
+                        Console.WriteLine("{0}. meghívott: {1}. Bulira hajlandó: {2}  ", k, newList[i].nev, newList[i].buli);
+                    }
+                }
+                else if (Blist20.Count < 15) {
+                    for (int i = 0; i < Blist20.Count; i++)
+                    {
+                        k++;
+                        Console.WriteLine("{0}. meghívott: {1}. Bulira hajlandó: {2} ", k, newList[i].nev, newList[i].buli);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sajnos 10 fő kell a buli szervezéséhez");
+                }
+             }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BLapoz bl = new BLapoz();
+            bl.bekeres();
+            bl.Kiir();
+            Console.ReadKey();
+        }
+    }
+}
