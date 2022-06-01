b223ecc9928a65fbcb2725c22c86b3a03b4b1538
Levente Boros <2021291@dszcberegszaszi.hu>
2022-06-01 10:50:22 +0200

    asd

---
+++ formDinamic/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace formDinamic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblSzoveg = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(226)))));
+            this.groupBox1.Controls.Add(this.lblSzoveg);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(240)))));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Kattintson a form területén! Létrjön egy címke, rajta 1-100 terjedő random szám";
+            // 
+            // lblSzoveg
+            // 
+            this.lblSzoveg.AutoSize = true;
+            this.lblSzoveg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSzoveg.Location = new System.Drawing.Point(28, 47);
+            this.lblSzoveg.Name = "lblSzoveg";
+            this.lblSzoveg.Size = new System.Drawing.Size(433, 20);
+            this.lblSzoveg.TabIndex = 1;
+            this.lblSzoveg.Text = "Darabszám: 0. Összeg: 0. Átlag: 0. Minimum: 0. Maximum: 0.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblSzoveg;
+    }
+}
+
---
+++ formDinamic/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formDinamic
+{
+    public partial class Form1 : Form
+    {
+        double atlag; int osszeg, db, min = int.MaxValue, max = int.MinValue;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Random random = new Random();
+            int rnd = random.Next(100) + 1;
+            Label label = new Label();
+            label.Location = new Point(e.X, e.Y);
+            label.Text = rnd.ToString();
+            label.AutoSize = true;
+            int red = random.Next(0, 256), green = random.Next(0, 256), blue= random.Next(0,256);
+            label.BackColor = Color.FromArgb(red, green, blue);
+            Controls.Add(label);
+            db++;
+            osszeg += rnd;
+            atlag = osszeg / (double)db;
+            if(min > rnd ) min = rnd; 
+            if(max<rnd ) max = rnd;
+            lblSzoveg.Text = string.Format("Darabszám :   {0} ,   összeg :   {1} ,   átlag :   {2} , " + "minimum :   {3} ,  maximum:   {4}. ", db, osszeg, atlag, min, max);
+        }
+    }
+}
