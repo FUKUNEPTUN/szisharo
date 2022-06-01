0414f9d9b3fb602828fb5febf69acb2d2d44d959
Boros Levente <88634980+FUKUNEPTUN@users.noreply.github.com>
2022-06-01 10:18:39 +0200

    Add files via upload

---
+++ formKepForog/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace formKepForog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBalra = new System.Windows.Forms.Button();
+            this.btnJobbra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.CadetBlue;
+            this.label4.Image = global::formKepForog.Properties.Resources.k;
+            this.label4.Location = new System.Drawing.Point(277, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 100);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.CadetBlue;
+            this.label5.Image = global::formKepForog.Properties.Resources.h;
+            this.label5.Location = new System.Drawing.Point(440, 403);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 100);
+            this.label5.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.CadetBlue;
+            this.label6.Image = global::formKepForog.Properties.Resources.k;
+            this.label6.Location = new System.Drawing.Point(584, 341);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 100);
+            this.label6.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.CadetBlue;
+            this.label7.Image = global::formKepForog.Properties.Resources.h;
+            this.label7.Location = new System.Drawing.Point(635, 202);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 100);
+            this.label7.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.CadetBlue;
+            this.label8.Image = global::formKepForog.Properties.Resources.h;
+            this.label8.Location = new System.Drawing.Point(238, 202);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 100);
+            this.label8.TabIndex = 3;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.CadetBlue;
+            this.label9.Image = global::formKepForog.Properties.Resources.k;
+            this.label9.Location = new System.Drawing.Point(288, 341);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 100);
+            this.label9.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.CadetBlue;
+            this.label3.Image = global::formKepForog.Properties.Resources.k;
+            this.label3.Location = new System.Drawing.Point(573, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 100);
+            this.label3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.CadetBlue;
+            this.label1.Image = global::formKepForog.Properties.Resources.h;
+            this.label1.Location = new System.Drawing.Point(440, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 100);
+            this.label1.TabIndex = 2;
+            // 
+            // btnBalra
+            // 
+            this.btnBalra.AutoSize = true;
+            this.btnBalra.Image = global::formKepForog.Properties.Resources.kb;
+            this.btnBalra.Location = new System.Drawing.Point(847, 41);
+            this.btnBalra.Name = "btnBalra";
+            this.btnBalra.Size = new System.Drawing.Size(42, 61);
+            this.btnBalra.TabIndex = 1;
+            this.btnBalra.UseVisualStyleBackColor = true;
+            // 
+            // btnJobbra
+            // 
+            this.btnJobbra.AutoSize = true;
+            this.btnJobbra.Image = global::formKepForog.Properties.Resources.kn;
+            this.btnJobbra.Location = new System.Drawing.Point(12, 41);
+            this.btnJobbra.Name = "btnJobbra";
+            this.btnJobbra.Size = new System.Drawing.Size(49, 61);
+            this.btnJobbra.TabIndex = 0;
+            this.btnJobbra.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(901, 512);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBalra);
+            this.Controls.Add(this.btnJobbra);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJobbra;
+        private System.Windows.Forms.Button btnBalra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
