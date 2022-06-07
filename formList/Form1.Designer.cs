36f0fb233ace731b3742e47e9f6ce18ceb9022e1
Levente Boros <2021291@dszcberegszaszi.hu>
2022-06-07 13:47:45 +0200

    Form Lista

--- formList/Form1.Designer.cs
+++ formList/Form1.Designer.cs
@@ -31,90 +31,106 @@ namespace formList
         {
             this.LBx_a = new System.Windows.Forms.ListBox();
             this.LBx_b = new System.Windows.Forms.ListBox();
-            this.LBx_unio = new System.Windows.Forms.ListBox();
-            this.listBox4 = new System.Windows.Forms.ListBox();
-            this.listBox5 = new System.Windows.Forms.ListBox();
-            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.LBx_Unio = new System.Windows.Forms.ListBox();
+            this.LBx_Metsz = new System.Windows.Forms.ListBox();
+            this.LBx_A_B = new System.Windows.Forms.ListBox();
+            this.LBx_B_A = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.TBx_B = new System.Windows.Forms.TextBox();
             this.TBx_A = new System.Windows.Forms.TextBox();
             this.btnEloall = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // LBx_a
             // 
+            this.LBx_a.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.LBx_a.FormattingEnabled = true;
-            this.LBx_a.Location = new System.Drawing.Point(0, 0);
+            this.LBx_a.Location = new System.Drawing.Point(12, 0);
             this.LBx_a.Name = "LBx_a";
             this.LBx_a.Size = new System.Drawing.Size(96, 277);
             this.LBx_a.TabIndex = 0;
             // 
             // LBx_b
             // 
+            this.LBx_b.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.LBx_b.FormattingEnabled = true;
-            this.LBx_b.Location = new System.Drawing.Point(102, 0);
+            this.LBx_b.Location = new System.Drawing.Point(137, 0);
             this.LBx_b.Name = "LBx_b";
             this.LBx_b.Size = new System.Drawing.Size(96, 277);
             this.LBx_b.TabIndex = 0;
             // 
-            // LBx_unio
+            // LBx_Unio
             // 
-            this.LBx_unio.FormattingEnabled = true;
-            this.LBx_unio.Location = new System.Drawing.Point(204, 0);
-            this.LBx_unio.Name = "LBx_unio";
-            this.LBx_unio.Size = new System.Drawing.Size(96, 277);
-            this.LBx_unio.TabIndex = 0;
+            this.LBx_Unio.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LBx_Unio.FormattingEnabled = true;
+            this.LBx_Unio.Location = new System.Drawing.Point(271, 0);
+            this.LBx_Unio.Name = "LBx_Unio";
+            this.LBx_Unio.Size = new System.Drawing.Size(96, 277);
+            this.LBx_Unio.TabIndex = 0;
             // 
-            // listBox4
+            // LBx_Metsz
             // 
-            this.listBox4.FormattingEnabled = true;
-            this.listBox4.Location = new System.Drawing.Point(307, 0);
-            this.listBox4.Name = "listBox4";
-            this.listBox4.Size = new System.Drawing.Size(96, 277);
-            this.listBox4.TabIndex = 0;
+            this.LBx_Metsz.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LBx_Metsz.FormattingEnabled = true;
+            this.LBx_Metsz.Location = new System.Drawing.Point(407, 0);
+            this.LBx_Metsz.Name = "LBx_Metsz";
+            this.LBx_Metsz.Size = new System.Drawing.Size(96, 277);
+            this.LBx_Metsz.TabIndex = 0;
             // 
-            // listBox5
+            // LBx_A_B
             // 
-            this.listBox5.FormattingEnabled = true;
-            this.listBox5.Location = new System.Drawing.Point(409, 0);
-            this.listBox5.Name = "listBox5";
-            this.listBox5.Size = new System.Drawing.Size(96, 277);
-            this.listBox5.TabIndex = 0;
+            this.LBx_A_B.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LBx_A_B.FormattingEnabled = true;
+            this.LBx_A_B.Location = new System.Drawing.Point(544, 0);
+            this.LBx_A_B.Name = "LBx_A_B";
+            this.LBx_A_B.Size = new System.Drawing.Size(96, 277);
+            this.LBx_A_B.TabIndex = 0;
             // 
-            // listBox6
+            // LBx_B_A
             // 
-            this.listBox6.FormattingEnabled = true;
-            this.listBox6.Location = new System.Drawing.Point(511, 0);
-            this.listBox6.Name = "listBox6";
-            this.listBox6.Size = new System.Drawing.Size(96, 277);
-            this.listBox6.TabIndex = 0;
+            this.LBx_B_A.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LBx_B_A.FormattingEnabled = true;
+            this.LBx_B_A.Location = new System.Drawing.Point(680, 0);
+            this.LBx_B_A.Name = "LBx_B_A";
+            this.LBx_B_A.Size = new System.Drawing.Size(96, 277);
+            this.LBx_B_A.TabIndex = 0;
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 310);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.Size = new System.Drawing.Size(112, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Halmazok elemszáma";
+            this.label1.Text = "Halmazok elemszáma:";
             // 
             // TBx_B
             // 
+            this.TBx_B.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.TBx_B.Location = new System.Drawing.Point(15, 352);
             this.TBx_B.Name = "TBx_B";
-            this.TBx_B.Size = new System.Drawing.Size(100, 20);
+            this.TBx_B.Size = new System.Drawing.Size(109, 20);
             this.TBx_B.TabIndex = 2;
             // 
             // TBx_A
             // 
+            this.TBx_A.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.TBx_A.Location = new System.Drawing.Point(15, 326);
             this.TBx_A.Name = "TBx_A";
-            this.TBx_A.Size = new System.Drawing.Size(100, 20);
+            this.TBx_A.Size = new System.Drawing.Size(109, 20);
             this.TBx_A.TabIndex = 3;
             // 
             // btnEloall
             // 
-            this.btnEloall.Location = new System.Drawing.Point(137, 350);
+            this.btnEloall.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnEloall.Location = new System.Drawing.Point(146, 350);
             this.btnEloall.Name = "btnEloall";
             this.btnEloall.Size = new System.Drawing.Size(75, 23);
             this.btnEloall.TabIndex = 4;
@@ -122,19 +138,85 @@ namespace formList
             this.btnEloall.UseVisualStyleBackColor = true;
             this.btnEloall.Click += new System.EventHandler(this.btnEloall_Click);
             // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "A halmaz";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "B halmaz";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(299, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Unió";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(428, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Metszet";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(541, 284);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 39);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Eleme \'A\' halmaznak,\r\nde nem eleme\r\n\'B halmaznak.";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(681, 284);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 39);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Eleme \'B\' halmaznak,\r\nde nem eleme\r\n\'A\'halmaznak.\r\n";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnEloall);
             this.Controls.Add(this.TBx_A);
             this.Controls.Add(this.TBx_B);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listBox6);
-            this.Controls.Add(this.LBx_unio);
-            this.Controls.Add(this.listBox5);
-            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.LBx_B_A);
+            this.Controls.Add(this.LBx_Unio);
+            this.Controls.Add(this.LBx_A_B);
+            this.Controls.Add(this.LBx_Metsz);
             this.Controls.Add(this.LBx_b);
             this.Controls.Add(this.LBx_a);
             this.Name = "Form1";
@@ -148,14 +230,20 @@ namespace formList
 
         private System.Windows.Forms.ListBox LBx_a;
         private System.Windows.Forms.ListBox LBx_b;
-        private System.Windows.Forms.ListBox LBx_unio;
-        private System.Windows.Forms.ListBox listBox4;
-        private System.Windows.Forms.ListBox listBox5;
-        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.ListBox LBx_Unio;
+        private System.Windows.Forms.ListBox LBx_Metsz;
+        private System.Windows.Forms.ListBox LBx_A_B;
+        private System.Windows.Forms.ListBox LBx_B_A;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox TBx_B;
         private System.Windows.Forms.TextBox TBx_A;
         private System.Windows.Forms.Button btnEloall;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- formList/Form1.cs
+++ formList/Form1.cs
@@ -16,11 +16,72 @@ namespace formList
         {
             InitializeComponent();
         }
-
+        Random random = new Random();
         private void btnEloall_Click(object sender, EventArgs e)
         {
             HalmazGeneral(LBx_a, Convert.ToInt32(TBx_A.Text));
-            HalmazGeneral(LBx_a, Convert.ToInt32(TBx_B.Text));
+            HalmazGeneral(LBx_b, Convert.ToInt32(TBx_B.Text));
+            Metszet(LBx_a, LBx_b, LBx_Metsz);
+            Unio(LBx_a, LBx_b, LBx_Unio);
+            Minusz(LBx_a, LBx_b, LBx_A_B);
+            Minusz(LBx_b, LBx_a, LBx_B_A);
+        }
+        private void HalmazGeneral(ListBox LB,  int N)
+        {
+            int elem;
+            LB.Items.Clear();
+            for(int i = 0; i < N; i++)
+            {
+                do
+                {
+                    elem = random.Next(N * 5);
+                } while (Bennevan(LB, elem));
+                LB.Items.Add(elem);
+            }
+        }
+        private bool Bennevan(ListBox LB, object elem)
+        {
+            for(int i = 0; i < LB.Items.Count; i++)
+                if(LB.Items[i] . Equals (elem )) return true;
+            return false;
+        }
+        private void Unio(ListBox LBx_A, ListBox LBx_B, ListBox LBx_Unio)
+        {
+            LBx_Unio.Items.Clear();
+            Unio_For(LBx_A, LBx_B, LBx_Unio);
+            Unio_For(LBx_B, LBx_A, LBx_Unio);
+        }
+        private void Unio_For(ListBox LBx, ListBox LBx1, ListBox LBx_Unio)
+        {
+            for (int i = 0; i < LBx.Items.Count; i++)
+            {
+                if (!Bennevan(LBx_Unio, LBx.Items[i]))
+                {
+                    LBx_Unio.Items.Add(LBx.Items[i]);
+                }
+            }
+        }
+        private void Metszet(ListBox LBx_A, ListBox LBx_B, ListBox LBx_Metsz)
+        {
+            LBx_Metsz.Items.Clear();
+            for(int i = 0; i < LBx_A.Items.Count; i++)
+            {
+                if(Bennevan (LBx_B, LBx_A.Items[i]))
+                {
+                    LBx_Metsz.Items.Add(LBx_A.Items[i]);
+                }
+            }
+        }
+        private void Minusz(ListBox LBx_A, ListBox LBx_B, ListBox LBx_Minusz)
+        {
+            LBx_Minusz.Items.Clear();
+            for (int i = 0; i < LBx_A.Items.Count; i++)
+            {
+                if (!Bennevan(LBx_B, LBx_A.Items[i]))
+                {
+                    LBx_Minusz.Items.Add(LBx_A.Items[i]);
+                }
+            }
         }
     }
 }
